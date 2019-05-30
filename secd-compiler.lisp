b844de5924836b7ac19b7cf386f69f8498b769de
myaosato <tetu60u@yahoo.co.jp>
2019-05-30 13:32:29 +0900

    add: let and variable

--- secd-compiler.lisp
+++ secd-compiler.lisp
@@ -19,12 +19,28 @@
 (defun compile-embedded-op (op args num names code)
   (append (compile-args args names num) (cons op code)))
 
+(defun compile-let-vars (list)
+  (if list
+      (cons (caar list) (compile-let-vars (cdr list)))
+      nil))
+
+(defun compile-let-exps (list)
+  (if list
+      (cons (cadar list) (compile-let-exps (cdr list)))
+      nil))
+
+(defun location (sym names)
+  (if (member sym (car names))
+      (cons 0 (position sym (car names)))
+      (let ((loc (location sym (cdr names))))
+        (cons (+ 1 (car loc)) (cdr loc)))))
+
 (defun compile-to-secd (exp names code)
   (if (atom exp)
       (cond ((integerp exp)
              (cons :ldc (cons exp code)))
             ((symbolp exp)
-             (cons :ldc (cons exp code)))
+             (cons :ld (cons (location exp names) code)))
             (t (error "~A is a not support type value" exp))) ;; TODO
       (let ((op (car exp))
             (args (cdr exp)))
@@ -44,6 +60,13 @@
                                           (cons (cadr exp) names) 
                                           (cons :rtn nil))))
                  (cons :ldf (cons cf code))))
+              ((eq (car exp) 'let) ;; letは新しい束縛の環境でbodyを即評価するってことかな
+               (let ((new-names (cons (compile-let-vars (cadr exp)) names))
+                     (args (compile-let-exps (cadr exp))))
+                 (let ((body (compile-to-secd (caddr exp)
+                                              new-names
+                                              (cons :rtn '()))))
+                   (compile-list args names (cons :ldf (cons body (cons :ap code)))))))
               (t (compile-list (cdr exp)
                                names
                                (compile-to-secd (car exp) names (cons :ap code))))))))
